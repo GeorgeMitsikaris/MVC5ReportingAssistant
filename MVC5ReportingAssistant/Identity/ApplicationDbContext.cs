f20cdd6dc67103184bce0399a4b2783ca5228bd9
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-04-27 19:53:59 +0300

    Initial Model

---
+++ MVC5ReportingAssistant/Identity/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC5ReportingAssistant.Identity
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ MVC5ReportingAssistant/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MVC5ReportingAssistant.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVC5ReportingAssistant.Identity.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MVC5ReportingAssistant.Identity.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
