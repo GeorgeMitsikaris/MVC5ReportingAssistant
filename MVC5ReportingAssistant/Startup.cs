631e0b8ce8c59afce0eef17653e19e34cc78bf81
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-04-27 22:13:04 +0300

    Creating users and roles. Adding Home controller and view

--- MVC5ReportingAssistant/Startup.cs
+++ MVC5ReportingAssistant/Startup.cs
@@ -2,6 +2,11 @@
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.Cookies;
+using MVC5ReportingAssistant.Identity;
 
 [assembly: OwinStartup(typeof(MVC5ReportingAssistant.Startup))]
 
@@ -11,7 +16,127 @@ namespace MVC5ReportingAssistant
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
 
+            CreateUsersAndRoles();
+        }
+
+        public void CreateUsersAndRoles()
+        {
+            //Creating role Admin
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>());
+            var db = new ApplicationDbContext();
+            var userStore = new ApplicationUserStore(db);
+            var userManager = new ApplicationUserManager(userStore);
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                roleManager.Create(new IdentityRole("Admin"));
+            }
+
+            //Adding user George
+            if (userManager.FindByName("George") == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = "George";
+                user.Email = "george@test.com";
+                var password = "George1";
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            //Adding user John
+            if (userManager.FindByName("John") == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = "John";
+                user.Email = "john@test.com";
+                var password = "John123";
+
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            //Adding user Mary
+            if (userManager.FindByName("Mary") == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = "Mary";
+                user.Email = "mary@test.com";
+                string password = "Mary123";
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            //Creating User role
+            if (!roleManager.RoleExists("User"))
+            {
+                roleManager.Create(new IdentityRole("User"));
+            }
+
+            //Creating user Sam
+            if (userManager.FindByName("Sam") == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = "Sam";
+                user.Email = "sam@test.com";
+                string password = "Sam1234";
+
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "User");
+                }
+            }
+
+            //Creating user Ben
+            if (userManager.FindByName("Ben") == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = "Ben";
+                user.Email = "ben@test.com";
+                string password = "Ben1234";
+
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "User");
+                }
+            }
+
+            //Creating user Ann
+            if (userManager.FindByName("Ann") == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = "Ann";
+                user.Email = "ann@test.com";
+                string password = "Ann1234";
+
+                var checkUser = userManager.Create(user, password);
+
+                if (checkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "User");
+                }
+            }
         }
     }
 }
