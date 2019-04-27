f564e0735f626ae5d167b17b606412eabb57e806
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-04-27 20:19:27 +0300

    Creating OWIN Startup class

---
+++ MVC5ReportingAssistant/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MVC5ReportingAssistant.Startup))]
+
+namespace MVC5ReportingAssistant
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
