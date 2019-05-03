1a1688f2e7b1962926c3ba3122148472f5f7ddbd
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-03 16:58:31 +0300

    Creating RegisterViewModel

---
+++ MVC5ReportingAssistant/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5ReportingAssistant.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
