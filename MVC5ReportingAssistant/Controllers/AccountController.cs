e5db315d51c51f006ff06cee6ee963f40ebe7ae3
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-05 17:41:01 +0300

    Finishing Register page

--- MVC5ReportingAssistant/Controllers/AccountController.cs
+++ MVC5ReportingAssistant/Controllers/AccountController.cs
@@ -1,21 +1,19 @@
-﻿using MVC5ReportingAssistant.Identity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using MVC5ReportingAssistant.Identity;
 using MVC5ReportingAssistant.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace MVC5ReportingAssistant.Controllers
 {
     public class AccountController : Controller
     {
-        private ApplicationDbContext _db;
-
-        public AccountController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
+        private ApplicationDbContext _db = new ApplicationDbContext();
 
         // GET: Account
         public ActionResult Register()
@@ -23,10 +21,43 @@ namespace MVC5ReportingAssistant.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Register(RegisterViewModel model)
-        //{
-           
-        //}
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Register User
+                var userStore = new ApplicationUserStore(_db);
+                var userManager = new ApplicationUserManager(userStore);
+                var hashPassword = Crypto.HashPassword(model.Password);
+                var user = new ApplicationUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    PasswordHash = hashPassword,
+                    PhoneNumber = model.PhoneNumber
+                };
+
+                var isRegisterSuccessfull = userManager.Create(user);
+
+                if (isRegisterSuccessfull.Succeeded)
+                {
+                    //Add role to User
+                    userManager.AddToRole(user.Id, "User");
+
+                    //Login User
+                    var authenticationManager = HttpContext.GetOwinContext().Authentication;
+                    var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    authenticationManager.SignIn(new AuthenticationProperties(), userIdentity);
+                }
+
+                return RedirectToAction("Index", "Home"); 
+            }
+            else
+            {
+                ModelState.AddModelError("MyError", "Invalid data");
+                return View();
+            }
+        }
     }
 }
